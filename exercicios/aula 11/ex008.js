5f3d36e5423d20bd6d82b52abcdd2d6706b4dd70
DiegoSPrado <159810695+DiegoSPrado@users.noreply.github.com>
2024-03-14 16:04:47 -0300

    fiz varias coisas

---
+++ exercicios/aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`a velocidade do seu veículo é de ${vel} Km/h.`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade! MULTADO!`)
+}
+console.log(`Siga viagem e volte sempre!`)
