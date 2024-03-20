021b6a6611f5dcf3dd47aef8d17a3f43f1507ad5
DiegoSPrado <159810695+DiegoSPrado@users.noreply.github.com>
2024-03-20 18:46:42 -0300

    ex 1 de repetiçoes

---
+++ exercicios/aula 14/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 3)
+
+/*var c = 1 
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+
+//console.log(`Tudo Bem?`)
