db7e8ad1c803dd21dd3ac2d052887a2ee8daaa42
DiegoSPrado <159810695+DiegoSPrado@users.noreply.github.com>
2024-03-26 10:35:12 -0300

    ex 17 finish

--- exercicios/ex 17/estilos/script.js
+++ exercicios/ex 17/estilos/script.js
@@ -1,4 +1,17 @@
 function gerar() {
     var num = document.querySelector('input#inum')
     var tab = document.querySelector('select#itab')
+
+    if (num.value.length == 0){
+        window.alert('Digite um número para a tabuada.')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(let c = 1; c <= 10; c++){
+            let item  = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
 }
